00ec03624f832f5814c0c33ad88c16c78ea64259
jbares <dead_aim_5@hotmail.com>
2016-09-20 18:06:28 +1000

    rename again under new project

---
+++ Converter/Converter/Converter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Converter.Services;
+
+namespace Converter
+{
+    class Converter
+    {
+        static void Main(string[] args)
+        {
+            ConverterService converter = new ConverterService();
+
+            //Infinite loop
+            while (true)
+            {
+                Console.WriteLine("Please input a value:");
+                string input = Console.ReadLine();
+                Console.WriteLine(converter.OutputMessage(input));
+            }
+        }
+    }
+}
