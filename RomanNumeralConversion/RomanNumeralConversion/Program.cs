c316224a176012d8e8dc70f0d2dd2df104628a66
jbares <dead_aim_5@hotmail.com>
2016-09-20 16:42:32 +1000

    added translations and material value

--- RomanNumeralConversion/RomanNumeralConversion/Program.cs
+++ RomanNumeralConversion/RomanNumeralConversion/Program.cs
@@ -11,29 +11,65 @@ namespace RomanNumeralConversion
     {
         static void Main(string[] args)
         {
-            string inputMsg = "Please input a value in roman numerals:";
-            while(true)
-            { 
-                Console.WriteLine(inputMsg);
-                string input = Console.ReadLine().ToUpper();
-                Regex rgx = new Regex("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+            Regex rgx = new Regex("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$");
+            Dictionary<string, string> translation = new Dictionary<string, string>();
+            translation.Add("I", "glob");
+            translation.Add("V", "prok");
+            translation.Add("X", "pish");
+            translation.Add("L", "tegj");
+            Dictionary<string, double> material = new Dictionary<string, double>();
+            material.Add("silver", 17);
+            material.Add("gold", 14450);
+            material.Add("iron", 195.5);            
 
-                if(input.ToLower() == "exit")
+            string inputMsg = "Please input a value:";
+
+            while (true)
+            {
+                Console.WriteLine(inputMsg);
+                string input = Console.ReadLine().ToLower();
+                string outputMessage = input;
+                double materialValue = 1;
+
+                foreach (KeyValuePair<string, string> entry in translation)
+                {
+                    input = input.Replace(entry.Value, entry.Key);
+                }
+                foreach (KeyValuePair<string, double> entry in material)
+                {
+                    if(input.Contains(entry.Key))
+                    {
+                        materialValue = entry.Value;
+                        input = input.Replace(entry.Key, "");
+                    }
+                }
+
+                input = Regex.Replace(input, @"\s+", "");
+
+                if (input.ToLower() == "exit")
                 {
                     break;
-                }                
-                else if(!rgx.IsMatch(input) || string.IsNullOrEmpty(input))
+                }
+                else if (!rgx.IsMatch(input) || string.IsNullOrEmpty(input))
                 {
                     inputMsg = "Invalid input, please try again:";
                     continue;
                 }
                 else
                 {
-                    inputMsg = "Please input a value in roman numerals:";
+                    inputMsg = "Please input a value:";
                 }
-                 
-                 int inputValue = ConvertRNToInt(input);
-                 Console.WriteLine("Converted: " + inputValue.ToString());                                              
+
+                int outputValue = ConvertRNToInt(input);
+                if(materialValue > 1)
+                {
+                    double credits = outputValue * materialValue;
+                    Console.WriteLine(outputMessage + " is " + credits.ToString("n2") + " Credits");
+                }
+                else
+                {                    
+                    Console.WriteLine(outputMessage + " is " + outputValue.ToString("n2"));
+                }                
             }
         }
 
@@ -43,9 +79,9 @@ namespace RomanNumeralConversion
             List<char> charList = new List<char>(array);
             List<int> intList = new List<int>();
 
-            foreach(char x in charList)
+            foreach (char x in charList)
             {
-                switch(x.ToString())
+                switch (x.ToString())
                 {
                     case "I":
                         intList.Add(1);
@@ -67,17 +103,17 @@ namespace RomanNumeralConversion
                         break;
                     case "M":
                         intList.Add(1000);
-                        break;                    
+                        break;
                 }
             }
-           
-           int result = 0;
 
-            for(int i = 0; i < intList.Count; i++)
-            {                
-                if((i != intList.Count - 1) && intList.Count >= 1)
+            int result = 0;
+
+            for (int i = 0; i < intList.Count; i++)
+            {
+                if ((i != intList.Count - 1) && intList.Count >= 1)
                 {
-                    if(intList[i] < intList[i + 1])
+                    if (intList[i] < intList[i + 1])
                     {
                         result -= intList[i];
                     }
